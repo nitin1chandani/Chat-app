654deec0f33e499185ba05581a21d1adcaad5fa3
Nitin Chandani <nitin1chandani@gmail.com>
2023-04-19 00:01:28 +0530

    frontend added

---
+++ frontend/src/Pages/ChatPage.js
@@ -0,0 +1,16 @@
+import React, { useEffect } from "react";
+import axios from "axios";
+const ChatPage = () => {
+  const fetchChats = async () => {
+    const { data } = await axios.get("/api/chat");
+    console.log(data);
+  };
+
+  useEffect(() => {
+    fetchChats();
+  }, []);
+
+  return <div></div>;
+};
+
+export default ChatPage;
